81a19433f69ebf46998dbd7053ad59962f51178d
Noa Eckstein <noaeckstein@Noas-MBP.Dlink>
2021-12-24 23:25:01 +0200

    add overlay

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+"use strict";
+
 //Global variables - they stay alive and reflect the state of the game 
 let elPreviousCard = null;
 let flippedCouplesCount = 0;
-let startTime = 0;
+// let startTime = 0;
 let TOTAL_COUPLES_COUNT = 6;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+let cron;
+let missMatchCounter = 0;
 
 function cardClicked(elCard) {
-    
-    if (startTime === 0) {// Clock starts after user clicks the first card
-        startTime = Date.now();
-        console.log(startTime);
-    }
+    showReplayBtn('start-over');
+    start();// Clock starts after user clicks the first card
+    // if (startTime === 0) {// Clock starts after user clicks the first card
+    //     startTime = Date.now();
+    //     console.log(startTime);
+    // }
 
     if (elCard.classList.contains('flipped')) {// If the user clicked an already flipped card - do nothing and return from the function
         return;
@@ -17,7 +26,6 @@ function cardClicked(elCard) {
 
     elCard.classList.add('flipped');// Flip it
 
-    
     if (elPreviousCard === null) {// This is a first card, only keep it in the global variable
         elPreviousCard = elCard;
     } else {
@@ -25,6 +33,7 @@ function cardClicked(elCard) {
         let card2 = elCard.getAttribute('data-card');
 
         if (card1 !== card2) {// No match, schedule to flip them back in 1 second
+            addMissed();
             const DOM_TREE = document.querySelectorAll('.card');
             DOM_TREE.forEach(node => node.classList.add('disable'))
             setTimeout(function () {
@@ -39,14 +48,13 @@ function cardClicked(elCard) {
             elPreviousCard = null;
 
             if (TOTAL_COUPLES_COUNT === flippedCouplesCount) {// All cards flipped
-
-                toggle_visibility('startOver');// Show replay button  
-
-                let endTime = Date.now();
-
-                let currentGameDurationCal = endTime - startTime;
-                let currentGameDurationInt = parseInt(currentGameDurationCal);
-                document.querySelector(".bestscore").innerHTML = `Score: ${currentGameDurationInt}`; //Display Score
+                pause();//clock pause
+                showReplayBtn('start-over');  
+                showOverlay();
+                // let endTime = Date.now();
+                // let currentGameDurationCal = endTime - startTime;
+                // let currentGameDurationInt = parseInt(currentGameDurationCal);
+                // document.querySelector(".bestscore").innerHTML = `Score: ${currentGameDurationInt}`; //Display Score
             }
 
 
@@ -56,22 +64,98 @@ function cardClicked(elCard) {
 
 }
 
-function toggle_visibility(id) {
-    let e = document.getElementById(id);
-    if (e.style.visibility == 'visible') {
-        e.style.visibility = 'hidden'
-    } else {
-        e.style.visibility = 'visible';
-    }
+function addMissed(){
+    missMatchCounter ++;
+    document.querySelector('.mistakes').innerText = missMatchCounter;
 }
 
-function reset() {
-    flippedCouplesCount = 0;
-    startTime = 0;
-    toggle_visibility('startOver');
+function clearMissed(){
+    missMatchCounter = 0;
+    document.querySelector('.mistakes').innerText = missMatchCounter;
+}
+function showReplayBtn(id){
+    let e = document.querySelector('.'+id);
+    e.style.visibility = 'visible';
+}
 
+function hideReplayBtn(id){
+    let e = document.querySelector('.'+id);
+    e.style.visibility = 'hidden';
+}
+
+function showOverlay() {
+    document.querySelector('.overlay').style.display = 'block';
+  }
+  
+  function hideOverlay() {
+    document.querySelector('.overlay').style.display = 'none';
+  }
+// function toggle_visibility(id) {
+//     let e = document.getElementById(id);
+//     if (e.style.visibility === 'visible') {
+//         e.style.visibility = 'hidden'
+//     } else {
+//         e.style.visibility = 'visible';
+//     }
+// }
+
+function reset(self) {
+    if(self){document.querySelector('.overlay').style.display = 'none'};
+    flippedCouplesCount = 0;
+    // startTime = 0;
+    // toggle_visibility('start-over');
+    resetClock();
+    clearMissed();
     let cardElements = document.querySelectorAll('.card');// Unflip all cards
     for (let i = 0; i < cardElements.length; ++i) {
         cardElements[i].classList.remove('flipped');
     }
 }
+
+
+//Timer
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function resetClock(){
+    clearInterval(cron);
+    document.getElementById('hour').innerText = '00';
+    document.getElementById('minute').innerText = '00';
+    document.getElementById('second').innerText = '00';
+    document.getElementById('millisecond').innerText = '000';
+}
+ 
+ document.getElementById('hour').innerText = '00';
+  document.getElementById('minute').innerText = '00';
+  document.getElementById('second').innerText = '00';
+  document.getElementById('millisecond').innerText = '000';
+
+
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
+  document.getElementById('hour').innerText = returnData(hour);
+  document.getElementById('minute').innerText = returnData(minute);
+  document.getElementById('second').innerText = returnData(second);
+  document.getElementById('millisecond').innerText = returnData(millisecond);
+}
+
+function returnData(input) {
+    return input > 10 ? input : `0${input}`
+  }
